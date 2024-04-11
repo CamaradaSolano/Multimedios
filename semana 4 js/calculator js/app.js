5346ef9be0401ae4879749e2901905ad61b1df32
Solanito97 <bjsolano97@gmail.com>
2024-04-10 18:33:38 -0600

    add js

--- semana 4 js/calculator js/app.js
+++ semana 4 js/calculator js/app.js
@@ -1,2 +1,36 @@
-const number1 = 0;
-const number2 = 0;
+function sum() {
+    let a = parseInt(document.getElementById("dato1").value);
+    let b = parseInt(document.getElementById("dato2").value);
+    let resultado = a + b;
+    document.getElementById("resultado").innerHTML = resultado;
+  }
+  
+  function rest() {
+    let a = parseInt(document.getElementById("dato1").value);
+    let b = parseInt(document.getElementById("dato2").value);
+    let resultado = a - b;
+    document.getElementById("resultado").innerHTML = resultado;
+  }
+  
+  function multipli() {
+    let a = parseInt(document.getElementById("dato1").value);
+    let b = parseInt(document.getElementById("dato2").value);
+    let resultado = a * b;
+    document.getElementById("resultado").innerHTML = resultado;
+  }
+  
+  function divide() {
+    let a = parseInt(document.getElementById("dato1").value);
+    let b = parseInt(document.getElementById("dato2").value);
+    let resultado = a / b;
+    document.getElementById("resultado").innerHTML = resultado;
+  }
+
+  function reset() {
+    // Limpiar los campos de entrada y el resultado
+    document.getElementById("dato1").value = "";
+    document.getElementById("dato2").value = "";
+    document.getElementById("resultado").innerHTML = "";
+}
+
+  
