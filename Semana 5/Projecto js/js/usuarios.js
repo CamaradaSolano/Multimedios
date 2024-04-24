81ce1d56ffe0ea0574a4c9627c70504e9bd4cff7
Solanito97 <bjsolano97@gmail.com>
2024-04-24 09:26:46 -0600

    add delete and update users

--- Semana 5/Projecto js/js/usuarios.js
+++ Semana 5/Projecto js/js/usuarios.js
@@ -2,6 +2,8 @@
 let datosTabla = document.querySelector('#datos');//Cuando es queryselector requiere el #
 let mensajesSistema =  document.querySelector('#mensajesSistema');
 let formulario = document.getElementById('formulario');//Cuando es id, no requiere el #
+let formularioEditar = document.getElementById('formularioEditar');
+
 let nombrePagina = document.title;
 let nombreModuloListar = "Usuarios";
 let nombreModuloCrear = "Crear Usuarios";
@@ -9,15 +11,16 @@ let nombreModuloCrear = "Crear Usuarios";
 let url = "https://paginas-web-cr.com/Api/apis/";
 let listar = "ListaUsuarios.php";
 let insertar = "InsertarUsuarios.php";
+let actualizar = "ActualizarUsuarios.php";
+
 // ocupo json
-// leer el api
-//https://paginas-web-cr.com/Api/apis/ListaUsuarios.php
 
 let spinner = `
 <button class="btn btn-success" type="button" disabled>
     <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
     <span role="status">Loading...</span>
 </button>`;
+
 //Propiedades Fin
 
 
@@ -26,7 +29,7 @@ if ( nombrePagina == nombreModuloCrear ){
     formulario.addEventListener('submit', 
     function(evento) {
         evento.preventDefault();//evita que la pagina se recargue
-
+        
         let datos = new FormData(formulario);
         
         let datosEnviar = {
@@ -49,16 +52,38 @@ if ( nombrePagina == nombreModuloCrear ){
         })
         .catch(console.log)
 
-        //console.log(datosEnviar);
-        // apis/InsertarUsuarios.php
-        // { "name" : "marioaje", "password":"1234567890", "email":"marioaje@gmail.com" }     
-
-        //console.log(datos.get('email'));
-        //estos son iguales
-        //email = document.getElementById("email").value;
-       // alert("1");
     })
 }
+
+
+formularioEditar.addEventListener('submit', 
+function(evento) {
+    evento.preventDefault();//evita que la pagina se recargue
+    
+    let datos = new FormData(formularioEditar);
+
+    let datosEnviar = {
+        name: datos.get('name'),
+        password: datos.get('password'),        
+        id: datos.get('id')
+    }
+    console.log(datosEnviar);
+
+        //url + insertar esto es la url del servicio concatenada
+        fetch( url + actualizar,
+            {
+                method: 'POST',
+                body: JSON.stringify(datosEnviar)
+            } 
+        )
+        .then(respuesta=>respuesta.json())
+        .then( (datosrepuesta) => {
+            mensajeActualizar(datosrepuesta)
+        })
+        .catch(console.log)
+
+ 
+})
 
 //Eventos Fin
 
@@ -95,7 +120,42 @@ function mensajeInsertar(datos){
     }
 }
 
+function mensajeActualizar(datos){
+    if(datos.code == 200){        
+        mensajesSistema.innerHTML = `<div
+                class="alert alert-success alert-dismissible fade show"
+                role="alert"
+            >
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                ></button>
+                <strong>Actualizacion exitosa</strong>
+            </div>`;
+
+        setTimeout(cargarDatos, 3000);    
+    }
+    else{
+        mensajesSistema.innerHTML = `<div
+                class="alert alert-danger alert-dismissible fade show"
+                role="alert"
+            >
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                ></button>
+                <strong>Error al actualizar</strong>
+            </div>`;
+    }
+}
+
+
 function cargarDatos(){
+    datosTabla.innerHTML = "";
     loadspinner();
     
     //url + listar esto es la url del servicio concatenada
@@ -113,7 +173,7 @@ function mostrarDatos(datos){
     
     if(datos.code == 200){
         for (const iterator of datos.data) {  
-        
+        //onclick="editar('${iterator.id}', '${iterator.name}', '${iterator.password}', '${iterator.email}')"
             datosTabla.innerHTML += `
                 <tr class="">
                     <td>
@@ -121,7 +181,7 @@ function mostrarDatos(datos){
                         name=""
                         id=""
                         class="btn btn-success"
-                        onclick="editar('${iterator.id}', '${iterator.name}', '${iterator.password}', '${iterator.email}')"
+                        onclick="editar('${encodeURIComponent(JSON.stringify(iterator))}')"
                         role="button"
                         ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg></a
                     >                    
@@ -131,6 +191,7 @@ function mostrarDatos(datos){
                         id=""
                         class="btn btn-danger"                        
                         role="button"
+                        onclick="eliminar('${iterator.id}')"
                         ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-eraser"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 20h-10.5l-4.21 -4.3a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9.2 9.3" /><path d="M18 13.3l-6.3 -6.3" /></svg></a
                     >
                     </td>
@@ -153,14 +214,35 @@ function loadspinner(){
     document.getElementById("spinnerload").innerHTML = spinner;
 }
 //onclick="editar('${iterator.id}', '${iterator.name}', '${iterator.password}', '${iterator.email}')"
-function editar(id, name, password, email) {
-    console.log(id);
-    console.log(name);
-    console.log(password);
-    console.log(email);
-   //no me sirvio console.log( JSON.stringify(objeto));
-   //console.log( JSON.parse(objeto));
+//function editar(id, name, password, email) {
+function editar(objeto) {
+    let modalEditar = new bootstrap.Modal(document.getElementById("modalEditar"));
+    modalEditar.show();
+
+    
+    objeto = JSON.parse(decodeURIComponent(objeto));
+    
+    document.getElementById("id").value = objeto.id;
+    document.getElementById("name").value = objeto.name;
+    document.getElementById("email").value = objeto.email;
+    document.getElementById("password").value = "";
+    document.getElementById("ideditar").innerHTML = objeto.id;
+
+
 }
+
+function eliminar(id){
+    document.getElementById("idEliminar").innerHTML = id;
+    document.getElementById("idEliminarModal").value = id;
+    
+    let modalEliminar = new bootstrap.Modal(document.getElementById("modalEliminar"));
+    modalEliminar.show();
+}
+
+function modalEliminarConfirmacion(){
+    //document.getElementById("idEliminarModal").value
+}
+
 //Metodos Fin
 
 //Seccion de ejecucion de funciones
